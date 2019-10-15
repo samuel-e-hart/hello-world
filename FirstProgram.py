f912114bbe35ad245ecc1140cd87e7eac0dbbb0b
samuel-e-hart <56592142+samuel-e-hart@users.noreply.github.com>
2019-10-15 08:40:17 -0400

    Add files via upload

---
+++ FirstProgram.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 22 11:51:59 2017
+
+@author: SEHart
+
+"""
+
+echo "# hello-world" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/samuel-e-hart/hello-world.git
+git push -u origin master
+
+
+
+
+
+print('Hello World')
